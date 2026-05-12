2ba66951f0decd12d5ad71731d071523934abe09
shashilsravan <shashilsravan.ss.ss@gmail.com>
2026-05-12 12:33:02 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://shashilsravan.github.io/apiwiz-react-challenge/',
+  base: '/apiwiz-react-challenge/',
   build: {
     minify: 'esbuild',
     target: 'es2020',
